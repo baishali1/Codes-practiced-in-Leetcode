9c0b9c9cbdf05e7a53956a7618725fa64e5859b9
baishali1 <96946238+baishali1@users.noreply.github.com>
2022-03-03 15:57:24 +0530

    Added solution - LeetHub

--- Heap Sort - GFG/heap-sort.cpp
+++ Heap Sort - GFG/heap-sort.cpp
@@ -15,27 +15,23 @@ class Solution
     void heapify(int arr[], int n, int i)  
     {
       // Your Code Here
+      int largest=i;
+      int l=2*i+1;
+      int r=2*i+2;
       
-      int largest = i; 
-   int l = 2 * i + 1; 
-   int r = 2 * i + 2;
-
-   
-   if (l < n && arr[l] > arr[largest])
-       largest = l;
-
-   
-   if (r < n && arr[r] > arr[largest])
-       largest = r;
-
-   
-   if (largest != i) {
-       swap(arr[i], arr[largest]);
-
-       
-       heapify(arr, n, largest);
-   }
-   
+      if(l<n && arr[l]>arr[largest]){
+          largest=l;
+      }
+      if(r<n && arr[r]>arr[largest]){
+          largest=r;
+      }
+      
+      if(largest!=i){
+          swap(arr[i],arr[largest]);
+          heapify(arr,n,largest);
+      }
+      
+  
     }
 
     public:
@@ -44,9 +40,10 @@ class Solution
     { 
     // Your Code Here
     
-    for(int i = n/2 - 1; i >= 0; i--){
-         heapify(arr,n,i);
-     }
+    for(int i=n/2-1;i>=0;i--){
+        heapify(arr,n,i);
+    }
+
      
     }
 
@@ -56,12 +53,13 @@ class Solution
     void heapSort(int arr[], int n)
     {
         //code here
+        buildHeap(arr,n);
+        for(int i=n-1;i>0;i--){
+            swap(arr[0],arr[i]);
+            heapify(arr,i,0);
+        }
         
-         buildHeap(arr,n);
-       for(int i = n-1; i > 0; i--){
-           swap(arr[0],arr[i]);
-           heapify(arr,i,0);
-       }
+           
     }
 };
 
