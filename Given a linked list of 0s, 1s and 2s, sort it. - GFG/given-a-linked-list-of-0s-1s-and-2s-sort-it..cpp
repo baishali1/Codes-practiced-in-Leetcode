b825b4733b6b0dce5f0f6dc44fc8590eeaa66400
baishali1 <96946238+baishali1@users.noreply.github.com>
2022-03-05 14:50:24 +0530

    Added solution - LeetHub

--- Given a linked list of 0s, 1s and 2s, sort it. - GFG/given-a-linked-list-of-0s-1s-and-2s-sort-it..cpp
+++ Given a linked list of 0s, 1s and 2s, sort it. - GFG/given-a-linked-list-of-0s-1s-and-2s-sort-it..cpp
@@ -37,25 +37,50 @@ class Solution
         
         // Add code here
         
-        Node *a;
-        int count=0;
-        int i=0;
-        vector<int>b;
-        
-        for(a=head;a!=NULL;a=a->next){
-            b.push_back(a->data);
-        }
-        sort(b.begin(),b.end());
-        //for(i=0;i<b.size();i++){
-          //cout<<b[i]<<endl;
-        //}
-    
-        
-        for(a=head;a!=NULL;a=a->next){
-            a->data=b[i++];
-            
-        }
-        return(head);
+   int onecount=0;
+   int twocount=0;
+   int zerocount=0;
+   Node*temp=head;
+   while(temp!=NULL){
+      if(temp->data==0)
+      zerocount++;
+   
+       else if(temp->data==1)
+           onecount++;
+       
+       
+        else if(temp->data==2)
+       twocount++;
+       
+       temp=temp->next;
+      
+   }
+   
+   temp=head;
+   while(temp!=NULL){
+       
+       if(zerocount!=0){
+           temp->data=0;
+           zerocount--;
+       }
+      else if(onecount!=0){
+           temp->data=1;
+           onecount--;
+       }
+       else if(twocount!=0){
+           temp->data=2;
+           twocount--;
+       }
+       
+       temp=temp->next;
+       
+       
+       
+       
+   }
+   return head;
+       
+
         
     }
 };
