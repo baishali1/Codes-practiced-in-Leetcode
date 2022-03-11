e220225620fac9ee1ec19a8ba3099e9d62ce073e
baishali1 <96946238+baishali1@users.noreply.github.com>
2022-03-11 19:40:23 +0530

    Added solution - LeetHub

--- Check if string is rotated by two places - GFG/check-if-string-is-rotated-by-two-places.cpp
+++ Check if string is rotated by two places - GFG/check-if-string-is-rotated-by-two-places.cpp
@@ -12,48 +12,31 @@ class Solution
     bool isRotated(string str1, string str2)
     {
         // Your code here
-        
         int i,v;
         string s;
-        //v=str1.size();
-        
-        if (str1.size()!=str2.size()){
+        if(str1.size()!=str2.size()){
             return(false);
         }
-        
-        s=str1;
-        
-        //if (v%2==0) {
-            //for(i=1;i<v/2;i++){
-            
-            
-                str1.push_back(str1[0]);
-                str1.push_back(str1[1]);
-                str1.erase(str1.begin(),str1.begin()+2);
-                    if(str1==str2){
-                    return(true);
-                    }
-            
-                //}
-         //}
-        
-        //if (v%2==1){
-            //for(i=1;i<v;i++){
-                s.insert(s.begin(),s[s.size()-1]);
-                //v=s.size();
-                s.insert(s.begin(),s[s.size()-2]);
-                s.erase(s.end()-2,s.end());
-                //cout<<s<<endl;
-                    if(s==str2){
-                    return(true);
-                    }
-            //}
-        //}
-        if(str1!=str2 && s!=str2)
-        return(false);
+          s=str1;
+        str1.push_back(str1[0]);
+        str1.push_back(str1[1]);
+        str1.erase(str1.begin(),str1.begin()+2);
+        if (str1==str2){
+            return(true);
+        }
+        s.insert(s.begin(),s[s.size()-1]);
+        s.insert(s.begin(),s[s.size()-2]);
+        s.erase(s.end()-2,s.end());
+        if (s==str2){
+            return(true);
+        }
+        if(str1!=str2 && s!=str2){
+            return(false);
+        }
     }
 
 };
+
 
 // { Driver Code Starts.
 
