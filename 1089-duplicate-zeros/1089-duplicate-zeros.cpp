1bb7d4dbc6e2aa58036ae3fe70971746756cb614
baishali1 <96946238+baishali1@users.noreply.github.com>
2022-03-22 10:17:00 +0530

    Time: 62 ms (29.21%), Space: 9.8 MB (45.23%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        int i,n;
+        n=arr.size();
+        for(i=0;i<arr.size();i++){
+            if(arr[i]==0){
+                arr.insert(arr.begin()+i,0);
+                i++;
+            }
+        }
+        if(arr.size()>n){
+        arr.erase(arr.begin()+n,arr.end());
+        }
+    }
+};
