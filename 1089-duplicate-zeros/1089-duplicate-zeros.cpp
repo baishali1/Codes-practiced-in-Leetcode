4df325adb5297353d861b820d8e53186a44ec802
baishali1 <96946238+baishali1@users.noreply.github.com>
2022-03-23 20:03:32 +0530

    Time: 29 ms (44.74%), Space: 9.7 MB (44.98%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.cpp
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     void duplicateZeros(vector<int>& arr) {
+        
         int i,n;
         n=arr.size();
         for(i=0;i<arr.size();i++){
@@ -10,7 +11,8 @@ public:
             }
         }
         if(arr.size()>n){
-        arr.erase(arr.begin()+n,arr.end());
+            arr.erase(arr.begin()+n,arr.end());
         }
+        
     }
 };
