818718711845e080b929e1dd940ad4f732aed12c
baishali1 <96946238+baishali1@users.noreply.github.com>
2022-04-29 12:39:58 +0530

    Time: 31 ms (8.12%), Space: 15.6 MB (44.79%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        int i,j;
+        int i;
+        int num;
+        int a=0;
         vector<int>v;
-        for(i=0;i<numbers.size()-1;i++)
-        {
-            j=upper_bound(numbers.begin(),numbers.end(),target-numbers[i])-numbers.begin()-1;
-            if (j!=numbers.size() && target-numbers[i]==numbers[j] && i!=j)
-            {
+        
+        for(i=0;i<numbers.size();i++){
+            num=target-numbers[i];
+            //(upper_bound will give iterator/pointer and subtracting                             numbers.begin() will give index)
+            a=(upper_bound(numbers.begin(),numbers.end(),num)-numbers.begin())-1; 
+            if(numbers[a]==num){
                 v.push_back(i+1);
-                v.push_back(j+1);
-                break;
+                v.push_back(a+1);
+                return v;
             }
         }
-     return v;   
+        return v;  
     }
 };
